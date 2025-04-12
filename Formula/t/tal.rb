def66f166fa36e680fae8e17c57bf6fd325c1acb
Michael Cho <michael@michaelcho.dev>
2025-04-12 14:01:36 -0400

    tal: update test

--- Formula/t/tal.rb
+++ Formula/t/tal.rb
@@ -30,12 +30,26 @@ class Tal < Formula
   end
 
   def install
+    ENV.O0 if OS.linux?
     system "make", "linux"
     bin.install "tal"
     man1.install "tal.1"
   end
 
   test do
-    system bin/"tal", "/etc/passwd"
+    (testpath/"test.c").write <<~C
+      /***************************************************/
+      /* some text and so on                    */
+      /*       even more text                                   */
+      /*       foo, bar. bar bar.                   */
+      /***************************************************/
+    C
+    assert_equal <<~C, shell_output("#{bin}/tal -p 0 test.c")
+      /***************************************************/
+      /* some text and so on                             */
+      /*       even more text                            */
+      /*       foo, bar. bar bar.                        */
+      /***************************************************/
+    C
   end
 end
